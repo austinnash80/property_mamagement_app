0dea186bd25007e91024c5f3bfa9b974d41575da
Austin Nash <austinnash@Austins-MBP-2.attlocal.net>
2025-02-26 13:40:48 -0800

    Ensure babel-plugin-macros is installed and configured

--- babel.config.js
+++ babel.config.js
@@ -36,47 +36,18 @@ module.exports = function(api) {
         }
       ]
     ].filter(Boolean),
-    plugins: [
-      require.resolve('babel-plugin-macros'),
-      '@babel/plugin-syntax-dynamic-import',
-      isTestEnv && 'babel-plugin-dynamic-import-node',
-      '@babel/plugin-transform-destructuring',
-      [
-        '@babel/plugin-proposal-class-properties',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-object-rest-spread',
-        {
-          useBuiltIns: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-methods',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-property-in-object',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          helpers: false
-        }
-      ],
-      [
-        '@babel/plugin-transform-regenerator',
-        {
-          async: false
-        }
-      ]
-    ].filter(Boolean)
+plugins: [
+  'babel-plugin-macros',
+  '@babel/plugin-syntax-dynamic-import',
+  '@babel/plugin-transform-destructuring',
+  [
+    '@babel/plugin-proposal-class-properties',
+    { loose: true }
+  ],
+  [
+    '@babel/plugin-proposal-private-methods',
+    { loose: true }
+  ]
+].filter(Boolean)
   }
 }
