17f8c29f902255ee401b153271289354954eeff1
Austin Nash <austinnash@Austins-MBP-2.attlocal.net>
2025-02-26 10:22:18 -0800

    2024 update

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,14 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+const webpack = require('webpack')
+
+// Ensuring 'global' is available as the global object
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    global: require.resolve('global/window')
+  })
+);
 
 module.exports = environment.toWebpackConfig()
